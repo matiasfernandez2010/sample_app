57383decfd304b254d234acefa36599d4b0beebf
Matias Fernandez <matiasfernandez2010@gmail.com>
2014-07-12 01:28:33 -0400

    Improve Rspec code

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,46 +6,45 @@ describe "Static pages" do
 	describe "Home page" do 
 		before { visit root_path }
 
-		it { should have_content('Sample App') }
+		it { should have_selector('h1', text: 'Sample App') }
 		it { should have_title(full_title('')) }
 		it { should_not have_title('| Home') }
 	end
 
 	describe "Help page" do 
+		before { visit help_path }
 
-		it "should have the content 'Help'" do 
-			visit help_path
-			expect(page).to have_content('Help')
-		end
-
-		it "should have the title 'Help'" do 
-			visit help_path
-			expect(page).to have_title(full_title('Help'))
-		end
+		it { should have_selector('h1', text: 'Help') }
+		it { should have_title(full_title('Help')) }
 	end
 
 	describe "About page" do 
+		before { visit about_path }
 
-		it "should have the content 'About'" do 
-			visit about_path
-			expect(page).to have_content('About')
-		end
-
-		it "should have the title 'About'" do 
-			visit about_path
-			expect(page).to have_title(full_title('About Us'))
-		end
+		it { should have_selector('h1', text: 'About Us') }
+		it { should have_title(full_title('About Us')) }
 	end
 
 	describe "Contact page" do
-		it "should have the content 'Contact'" do 
-			visit contact_path
-			expect(page).to have_content('Contact')
-		end
+		before { visit contact_path }
 
-		it "should have the title 'Contact'" do 
-			visit contact_path
-			expect(page).to have_title(full_title('Contact Us'))
-		end		
+		it { should have_selector('h1', text: 'Contact Us') }
+		it { should have_title(full_title('Contact Us')) }	
 	end
+
+	it "should have the right links on the layout" do 
+		visit root_path
+		click_link "About"
+		expect(page).to have_selector('h1', text: "About Us")
+		click_link "Help"
+		expect(page).to have_selector('h1', text: "Help")
+		click_link "Contact"
+		expect(page).to have_selector('h1', text: "Contact")
+		click_link "Home"
+		click_link "Sign up now!"
+		expect(page).to have_selector('h1', text: "Sign up")
+		click_link "sample app"
+		expect(page).to have_selector('h1', text: "Sample App")
+	end
+
 end
